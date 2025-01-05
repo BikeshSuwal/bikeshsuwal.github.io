b24e9fdde6fe558aed028be05149c19c62881aad
Bikesh Suwal <bikeshsuwal776@gmail.com>
2025-01-05 20:32:29 +0545

    add all

---
+++ precache-manifest.6763bd6d5fede6f669a3716d1390b881.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c8eb617c31cd8cba1f6",
+    "url": "/static/css/main.029727ec.chunk.css"
+  },
+  {
+    "revision": "8c8eb617c31cd8cba1f6",
+    "url": "/static/js/main.54fdb209.chunk.js"
+  },
+  {
+    "revision": "ef596ee87d1781b14ae8",
+    "url": "/static/js/runtime~main.68481440.js"
+  },
+  {
+    "revision": "a2e5cad04a496deb6423",
+    "url": "/static/js/2.9d630efa.chunk.js"
+  },
+  {
+    "revision": "279e4fca54e5d8e57f31",
+    "url": "/static/js/3.33a557c1.chunk.js"
+  },
+  {
+    "revision": "69500c44c42e664808f8ff2c5790f367",
+    "url": "/index.html"
+  }
+];
